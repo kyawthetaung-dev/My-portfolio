5498bbab7d8458784f39544574f2e16eafe59668
Kyaw Thet Aung <110282304+kyawthetaung-dev@users.noreply.github.com>
2024-01-09 22:43:57 +0630

    update portfolio

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -1,26 +1,34 @@
 class ProjectUtils {
   static const List<String> banners = [
     "assets/projects/music.jpg",
-    "assets/projects/qr.png"
+    "assets/projects/qr.png",
+    "assets/projects/html.png",
+    "assets/projects/css.png"
   ];
 
  
 
   static const List<String> titles = [
     "Music Application and Dashboard",
-    "QR Code Generator"
+    "QR Code Generator",
+    "Banking and ATM System",
+    "Mail and SMS System"
 
    
   ];
 
   static const List<String> description = [
     "Music Application and Dashboard developed by using Flutter and Node.js",
-    "QR mini web application developed by html,CSS,javaScript"
+    "QR mini web application developed by html,CSS,javaScript",
+    "Developed by html,CSS,javaScript,Workbench",
+    "Developed by html,CSS,Bootstrap,ASP.NET"
     
   ];
 
   static const List<String> links = [
     "https://github.com/kyawthetaung-dev/Music-D.git",
-    "https://github.com/kyawthetaung-dev/QR.git"
+    "https://kyawthetaung-dev.github.io/QR/",
+    "",
+    ""
   ];
 }
